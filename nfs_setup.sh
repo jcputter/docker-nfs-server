dc35f0d3cb7acaf7d7f2f84fc11f7adb4fca427d
JC  Putter <jcputter@gmail.com>
2016-09-09 19:14:56 +0200

    add insecure option to support android

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -7,7 +7,7 @@ mounts="${@}"
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
   mkdir -p $src
-  echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
+  echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash,insecure)" >> /etc/exports
 done
 
 exec runsvdir /etc/sv
